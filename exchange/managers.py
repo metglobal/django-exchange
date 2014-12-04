d0890ed637deb8ec5b400835d5c816ec812b6c90
titusz <tp@py7.de>
2014-12-05 00:28:31 +0100

    backwards compatible get_query_set rename - fixes #25

--- exchange/managers.py
+++ exchange/managers.py
@@ -1,10 +1,19 @@
+import django
 from django.db import models
 
 
 class ExchangeRateManager(models.Manager):
-    def get_query_set(self):
-        return super(ExchangeRateManager, self).get_query_set()\
-            .select_related('source', 'target')
+
+    def get_queryset(self):
+        super_ = super(ExchangeRateManager, self)
+        if django.VERSION < (1, 7):
+            qs = super_.get_query_set()
+        else:
+            qs = super_.get_queryset()
+        return qs.select_related('source', 'target')
+
+    if django.VERSION < (1, 7):
+        get_query_set = get_queryset
 
     def get_rate(self, source_currency, target_currency):
         return self.get(source__code=source_currency,
